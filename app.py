a9d67896f075e815fcf3c6a1a8fe1adbed0dba46
Vinay Pratap Singh <55905301+singhvinay37@users.noreply.github.com>
2020-10-02 10:34:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+import random
+from textblob import TextBlob, Word
 #from sklearn.externals import joblib
 #import sklearn.externals.joblib as extjoblib
 import joblib
@@ -28,6 +30,25 @@ def predict():
         #vect = cv.transform(data).toarray()
 		my_prediction = clf.predict(vect)
 	return render_template('result.html',prediction = my_prediction)
+        
+	blob=TextBlob(text)
+        nouns=list()
+        for word,tag in blob.tags:
+            if tag == 'NN':
+                nouns.append(word.lemmatize())
+        display=[]
+        output=""
+        for item in random.sample(nouns,len(nouns)):  
+            word=Word(item)
+            if word not in display:
+                display.append(word.capitalize())
+                
+        for i in display:
+            if len(i) > 2:
+                output = output + " " + i
+            else:
+                output = ""
+	return render_template('result.html',prediction = my_prediction,summary = output)
 
 
 
